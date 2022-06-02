67dda8329aeac2f771206aac8cedd70b5ac4ce42
Reese <Reesegetsemails@protonmail.com>
2022-06-02 17:20:43 -0600

    help me git jesus

--- make.sh
+++ make.sh
@@ -2,6 +2,7 @@
 PROGRAM_NAME='txtToHTML.c'
 BINARY_NAME='txtToHTML'
 
+mkdir -p ./bin
 
 musl-clang -O3 -Wall -static "./src/$PROGRAM_NAME" -o "./bin/$BINARY_NAME"
 #clang -O3 -Wall "./src/$PROGRAM_NAME" -o "./bin/$BINARY_NAME"
