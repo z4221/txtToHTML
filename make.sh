71e09910277e3912f193bbda5a87d5c284647ceb
Reese <Reesegetsemails@protonmail.com>
2022-06-02 17:11:58 -0600

    ok that one commit did not work i am bad with the gits

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@ PROGRAM_NAME='txtToHTML.c'
 BINARY_NAME='txtToHTML'
 
 
-#musl-clang -O3 -Wall -static "./src/$PROGRAM_NAME" -o "./bin/$BINARY_NAME"
-clang -O3 -Wall "./src/$PROGRAM_NAME" -o "./bin/$BINARY_NAME"
+musl-clang -O3 -Wall -static "./src/$PROGRAM_NAME" -o "./bin/$BINARY_NAME"
+#clang -O3 -Wall "./src/$PROGRAM_NAME" -o "./bin/$BINARY_NAME"
 
